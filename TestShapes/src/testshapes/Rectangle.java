094bcc5b8a45ab98bc051ce8a3b0c1d15db0c4a8
Gabriel Secula <gabe.secula@gmail.com>
2018-01-29 13:34:52 -0500

    Update Rectangle.java
    The boi is encapsulated.

--- TestShapes/src/testshapes/Rectangle.java
+++ TestShapes/src/testshapes/Rectangle.java
@@ -3,12 +3,22 @@ package testshapes;
 
 
 public class Rectangle {
-    public double length=0;
-    public double width=0;
-    Rectangle(double l, double w) {
-        length = l;
-        width = w;
+    private double length;
+    private double width;
+    Rectangle() {
+        length = 1;
+        width = 1;
     }
-    double getArea() {return length*width;}
-    double getCircumference() {return 2*length + 2*width;}
+    Rectangle(double l, double w) {
+        if(l >= 0) length = l;
+        else length = 0;
+        if(w >= 0) width = w;
+        else width = 0;
+    }
+    public double getArea() {return length*width;}
+    public double getCircumference() {return 2*length + 2*width;}
+    public void setLength(double l) {if(l >= 0) length = l;}
+    public void setWidth(double w) {if(w >= 0) width = w;}
+    public double getLength() {return length;}
+    public double getWidth() {return width;}
 }
