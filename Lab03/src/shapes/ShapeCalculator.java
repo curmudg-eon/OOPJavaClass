4bd0aec474e6ac916aad0c9e09d05b3b5140ff64
Gab <gabe.secula@gmail.com>
2018-03-05 13:02:52 -0500

    Adding more for lab03

--- Lab03/src/shapes/ShapeCalculator.java
+++ Lab03/src/shapes/ShapeCalculator.java
@@ -11,6 +11,7 @@ import javax.swing.JComboBox;
  * @author 01748792
  */
 public class ShapeCalculator extends javax.swing.JFrame {
+    String output;
     Shape[] shapeList = {new Circle(), new Rectangle(), new Triangle(), new Cylinder(), new RectPrism(), new TriPrism(), new Sphere()};
     String[] shapeListStr = {"Circle", "Rectangle", "Triangle", "Cylinder", "RectPrism", "TriPrism", "Sphere"};
     /**
@@ -41,7 +42,8 @@ public class ShapeCalculator extends javax.swing.JFrame {
         txtInput3 = new javax.swing.JTextField();
         lblInput4 = new javax.swing.JLabel();
         txtInput4 = new javax.swing.JTextField();
-        jButton1 = new javax.swing.JButton();
+        btnCalculate = new javax.swing.JButton();
+        lblResults = new javax.swing.JLabel();
 
         javax.swing.GroupLayout jLayeredPane1Layout = new javax.swing.GroupLayout(jLayeredPane1);
         jLayeredPane1.setLayout(jLayeredPane1Layout);
@@ -68,19 +70,32 @@ public class ShapeCalculator extends javax.swing.JFrame {
         });
 
         jPanel1.setLayout(new java.awt.GridLayout(4, 2));
+
+        lblInput1.setText("Enter a radius");
         jPanel1.add(lblInput1);
 
         txtInput1.setMinimumSize(new java.awt.Dimension(30, 20));
         txtInput1.setPreferredSize(new java.awt.Dimension(30, 20));
         jPanel1.add(txtInput1);
+
+        lblInput2.setText("-");
         jPanel1.add(lblInput2);
         jPanel1.add(txtInput2);
+
+        lblInput3.setText("-");
         jPanel1.add(lblInput3);
         jPanel1.add(txtInput3);
+
+        lblInput4.setText("-");
         jPanel1.add(lblInput4);
         jPanel1.add(txtInput4);
 
-        jButton1.setText("jButton1");
+        btnCalculate.setText("Calculate");
+        btnCalculate.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnCalculateActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -95,10 +110,12 @@ public class ShapeCalculator extends javax.swing.JFrame {
                         .addComponent(cmbBoxOptions, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(layout.createSequentialGroup()
                         .addGap(89, 89, 89)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addComponent(jButton1)
-                            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))))
-                .addContainerGap(190, Short.MAX_VALUE))
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(lblResults)
+                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                                .addComponent(btnCalculate)
+                                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))))
+                .addContainerGap(146, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -110,8 +127,10 @@ public class ShapeCalculator extends javax.swing.JFrame {
                 .addGap(80, 80, 80)
                 .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jButton1)
-                .addContainerGap(73, Short.MAX_VALUE))
+                .addComponent(btnCalculate)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(lblResults)
+                .addContainerGap(61, Short.MAX_VALUE))
         );
 
         pack();
@@ -137,6 +156,50 @@ public class ShapeCalculator extends javax.swing.JFrame {
         if (shapeList[cmbBoxOptions.getSelectedIndex()] instanceof ThreeDObject) 
             lblInput4.setText("Enter a height");
     }//GEN-LAST:event_cmbBoxOptionsActionPerformed
+
+    private void btnCalculateActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCalculateActionPerformed
+        switch (cmbBoxOptions.getSelectedIndex()) {
+            case 0:
+                ((Circle)shapeList[0]).setRadius(Double.parseDouble(txtInput1.getText()));
+                output = "Perimeter: " + shapeList[0].getPerimeter() + "\nArea: " + shapeList[0].getArea();
+                break;
+            case 1:
+                ((Rectangle)shapeList[1]).setLength(Double.parseDouble(txtInput1.getText()));
+                ((Rectangle)shapeList[1]).setWidth(Double.parseDouble(txtInput2.getText()));
+                output = "Perimeter: " + shapeList[1].getPerimeter() + "\nArea: " + shapeList[1].getArea();
+                break;
+            case 2:
+                ((Triangle)shapeList[2]).setS1(Double.parseDouble(txtInput1.getText()));
+                ((Triangle)shapeList[2]).setS2(Double.parseDouble(txtInput2.getText()));
+                ((Triangle)shapeList[2]).setS3(Double.parseDouble(txtInput3.getText()));
+                output = "Perimeter: " + shapeList[2].getPerimeter() + "\nArea" + shapeList[2].getArea();
+                break;
+            case 3:
+                ((Cylinder)shapeList[3]).setRadius(Double.parseDouble(txtInput1.getText()));
+                ((Cylinder)shapeList[3]).setHeight(Double.parseDouble(txtInput4.getText()));
+                output = "Lateral Area : " + ((ThreeDObject)shapeList[3]).getLateralArea() + "\nSurface Area: " + ((ThreeDObject)shapeList[3]).getSurfaceArea() + "Volume: " + ((ThreeDObject)shapeList[3]).getVolume();
+                break;
+            case 4:
+                ((RectPrism)shapeList[4]).setLength(Double.parseDouble(txtInput1.getText()));
+                ((RectPrism)shapeList[4]).setWidth(Double.parseDouble(txtInput2.getText()));
+                ((RectPrism)shapeList[4]).setHeight(Double.parseDouble(txtInput4.getText()));
+                output = "Lateral Area : " + ((ThreeDObject)shapeList[4]).getLateralArea() + "\nSurface Area: " + ((ThreeDObject)shapeList[4]).getSurfaceArea() + "Volume: " + ((ThreeDObject)shapeList[4]).getVolume();
+                break;
+            case 5:
+                ((TriPrism)shapeList[5]).setS1(Double.parseDouble(txtInput1.getText()));
+                ((TriPrism)shapeList[5]).setS2(Double.parseDouble(txtInput2.getText()));
+                ((TriPrism)shapeList[5]).setS3(Double.parseDouble(txtInput3.getText()));
+                ((TriPrism)shapeList[5]).setHeight(Double.parseDouble(txtInput4.getText()));
+                output = "Lateral Area : " + ((ThreeDObject)shapeList[5]).getLateralArea() + "\nSurface Area: " + ((ThreeDObject)shapeList[5]).getSurfaceArea() + "Volume: " + ((ThreeDObject)shapeList[5]).getVolume();
+                break;
+            case 6:
+                ((Sphere)shapeList[6]).setRadius(Double.parseDouble(txtInput1.getText()));
+                output = "Surface Area: " + ((Sphere)shapeList[6]).getSurfaceArea() + "Volume: " + ((Sphere)shapeList[6]).getVolume();
+                break;
+        }
+        
+        lblResults.setText(output);
+    }//GEN-LAST:event_btnCalculateActionPerformed
 
     /**
      * @param args the command line arguments
@@ -174,8 +237,8 @@ public class ShapeCalculator extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnCalculate;
     private javax.swing.JComboBox<String> cmbBoxOptions;
-    private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLayeredPane jLayeredPane1;
     private javax.swing.JPanel jPanel1;
@@ -183,6 +246,7 @@ public class ShapeCalculator extends javax.swing.JFrame {
     private javax.swing.JLabel lblInput2;
     private javax.swing.JLabel lblInput3;
     private javax.swing.JLabel lblInput4;
+    private javax.swing.JLabel lblResults;
     private javax.swing.JTextField txtInput1;
     private javax.swing.JTextField txtInput2;
     private javax.swing.JTextField txtInput3;
